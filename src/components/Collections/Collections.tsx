36abf878a452802377fa3099bef228664bd14789
dinozin <dinozindev@gmail.com>
2024-10-23 21:03:57 -0300

    styling collections page

--- src/components/Collections/Collections.tsx
+++ src/components/Collections/Collections.tsx
@@ -7,135 +7,161 @@ const CollectionTitle = styled.h1`
     font-size: 60px;
 `
 
-const CollectionDiv = styled.div`
+const CollectionMain = styled.main`
     width: 100%;
     display: flex;
-    flex-wrap: wrap;
+    padding: 5rem 0;
+    margin: 0 auto;
     justify-content: center;
-    padding-top: 5rem;
 `
 
 const CollectionsDiv = styled.div`
-    width: 50%;
     display:flex;
     flex-wrap: wrap;
+    height: 70vh;
+    width: 20%;
+    overflow: auto;
+    gap: 0.5rem;
+    padding: 0.5rem;
 `
 
 const CollectionCard = styled.div`
+    text-align: center;
+    background-color: grey;
+    padding: 1rem;
+    display:flex;
+    justify-content: space-between;
+    border-radius: 8px;
+    height: 50px;
+    width: 100%;
+    text-align: center;
+    background-color: #AF75F9;
     p {
         margin-bottom: 1.25rem;
         font-weight: 700;
-        color: white;
+        color: black;
     }
-    text-align: center;
-    margin: 2rem;
-    background-color: grey;
-    padding: 2rem;
-    border-radius: 8px;
     &:hover {
         cursor: pointer;
-        scale: 1.1;
+        scale: 1.01;
         transition: 0.2s;
     }
-`
-
-const WeaponCard = styled.div`
-    background-color: lightgrey;
-    text-align:center; 
-    margin-bottom: 2.5rem;
-    padding: 2rem;
-    border-radius: 16px;
-    p {
-        font-size: 20px;
-        font-weight: 700;
-
+    img {
+        height: 90%;
     }
-`
-
-const CardSkinsDiv = styled.div`
-    display:flex;
-    flex-wrap: wrap;
-    flex-direction: column;
-
 `
 
 const CollectionDetails = styled.div`
     display:flex;
     flex-direction: column;
     align-items: center;
+    flex-wrap: wrap;
+    width: 70%;
+    justify-content: center;
 `
 
 const CollectionHeader = styled.header`
-    img {
-        margin-top: 2rem;
-    }
     text-align: center;
-    background-color: grey;
-    padding: 2rem;
-    width: 100%;
+    background-color: #281F32;
+    width: 90%;
+    padding: 2rem 0;
     border-radius: 8px;
     color: white;
     margin-bottom: 2rem;
+    height: 150px;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    img {
+        height: 90%;
+    }
 `
 
+const WeaponCard = styled.div`
+    background-color: lightgrey;
+    text-align:center; 
+    padding: 2rem;
+    border-radius: 16px;
+    width: 25%;
+    background-color: #4C495C;
+    p {
+        font-size: 20px;
+        font-weight: 700;
+    }
+    img {
+        width: 80%;
+    }
+`
+
+const CardSkinsDiv = styled.div`
+    display:flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 2rem;
+`
+
+
 const Collections = () => {
-  const { data, loading, error } = useFetchCollection('https://bymykel.github.io/CSGO-API/api/en/collections.json'); // API URL to connect to CSGO-API
-  const[collectionID, setCollectionID] = useState('collection-set-community-1');
+    const { data, loading, error } = useFetchCollection('https://bymykel.github.io/CSGO-API/api/en/collections.json'); // API URL to connect to CSGO-API
+    const [collectionID, setCollectionID] = useState('collection-set-community-1');
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
 
-  const filteredCollection = data?.filter(collection => !collection.name.includes('Graffiti')) // Removes graffiti collections
+    const filteredCollection = data?.filter(collection => !collection.name.includes('Graffiti')) // Removes graffiti collections
 
-  const handleFilteredCollection = (collection_id : string) => {  
+    const handleFilteredCollection = (collection_id: string) => {
         setCollectionID(collection_id)
-  }
+    }
 
-  const scrollParaTopo = () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    })
-  }
+    // const scrollParaTopo = () => {
+    //     window.scrollTo({
+    //         top: 0,
+    //         behavior: 'smooth'
+    //     })
+    // }
 
-  // quando clicar em uma coleção, esconder as demais e mostrar apenas a clicada.
-  return (
-    <>
-    {/* <pre>{JSON.stringify(data, null, 2)}</pre>  */}
-    <CollectionTitle>Collections</CollectionTitle>
-    <CollectionDiv>
-    <CollectionsDiv>
-      {filteredCollection?.map(collection => (
-        <CollectionCard key={collection.id} onClick={() => {handleFilteredCollection(collection.id); scrollParaTopo();}}>
-            <p>{collection.name}</p>
-            <img src={collection.image} alt="" />
-        </CollectionCard>
-      ))}
-      </CollectionsDiv>
-        {
-            filteredCollection?.filter(collection => collection.id === collectionID).map(collection => (
-                <CollectionDetails key={collection.id}>
-                    <CollectionHeader>
-                        <h1>{collection.name}</h1>
-                        <img src={collection.image} alt="" />
-                    </CollectionHeader>
-                    <CardSkinsDiv>
-                    {collection.contains.map(skin => (
-                        <WeaponCard key={skin.id}>
-                            <p>{skin.name}</p>
-                            <p style={{color: skin.rarity.color}}>{skin.rarity.name}</p>
-                            <img src={skin.image} alt="" />
-                        </WeaponCard>
+    //scrollParaTopo();
+
+    // quando clicar em uma coleção, esconder as demais e mostrar apenas a clicada.
+    return (
+        <>
+            {/* <pre>{JSON.stringify(data, null, 2)}</pre>  */}
+            <CollectionTitle>Collections</CollectionTitle>
+            <CollectionMain>
+                <CollectionsDiv>
+                    {filteredCollection?.map(collection => (
+                        <CollectionCard key={collection.id} onClick={() => { handleFilteredCollection(collection.id);}}>
+                            <p>{collection.name}</p>
+                            <img src={collection.image} alt={collection.name} />
+                        </CollectionCard>
                     ))}
-                    </CardSkinsDiv>
-                </CollectionDetails>
-            ))
-        }
-        </CollectionDiv>
-        
-    
-    </>
-  );
+                </CollectionsDiv>
+                {
+                    filteredCollection?.filter(collection => collection.id === collectionID).map(collection => (
+                        <CollectionDetails key={collection.id}>
+                            <CollectionHeader>
+                                <h1>{collection.name}</h1>
+                                <img src={collection.image} alt="" />
+                            </CollectionHeader>
+                            <CardSkinsDiv>
+                                {collection.contains.map(skin => (
+                                    <WeaponCard key={skin.id}>
+                                        <p>{skin.name}</p>
+                                        <p style={{ color: skin.rarity.color }}>{skin.rarity.name}</p>
+                                        <img src={skin.image} alt="" />
+                                    </WeaponCard>
+                                ))}
+                            </CardSkinsDiv>
+                        </CollectionDetails>
+                    ))
+                }
+            </CollectionMain>
+
+
+        </>
+    );
 };
 
 export default Collections;
--- src/components/MusicKits/MusicKits.tsx
+++ src/components/MusicKits/MusicKits.tsx
@@ -52,7 +52,7 @@ const MusicKits = () => {
         <MusicKitCard key={item.id}>
             <h2>{item.name}</h2>
             <p>{item.description}</p>
-            <img src={item.image} alt="" />
+            <img src={item.image} alt={item.name} />
         </MusicKitCard>
       ))}
     </MusicKitDiv>
